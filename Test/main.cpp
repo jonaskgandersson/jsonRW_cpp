2d179446ea91300d23ff41de37f386cd5e106e60
Jonas Andersson <jonaskgandersson@gmail.com>
2018-11-28 22:14:55 +0100

    add test code

---
+++ Test/main.cpp
@@ -0,0 +1,22 @@
+#include "jsonRW.hpp"
+
+#define BUFFER_LEN 1000
+
+int main( int argc, char* argv[])
+{
+    char buffer[BUFFER_LEN];
+    int err = JWRITE_OK;
+
+    jWrite jw( buffer, BUFFER_LEN );        // Create jWrite instance to use application buffer 
+    jw.open( JW_OBJECT, JW_PRETTY );    // open root node as object 
+    jw.obj_string( "key", "value" );    // writes "key":"value" 
+    jw.obj_int( "int", 1 );             // writes "int":1 
+    jw.obj_array( "anArray");           // start "anArray": [...] 
+    jw.arr_int( 0 );                 // add a few integers to the array 
+    jw.arr_int( 1 ); 
+    jw.arr_int( 2 ); 
+    jw.end();                           // end the array 
+    err= jw.close();                    // close root object - done
+
+    return 0;
+}
